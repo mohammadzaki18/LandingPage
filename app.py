f5d07d4d8bb4c16daf44f5558b5387f439590e54
mohammadzaki18 <mohammadzakijack@gmail.com>
2023-12-06 02:55:24 +0700

    Update
    Menambahkan foto 3 anggota

--- app.py
+++ app.py
@@ -27,10 +27,34 @@ st.write("""
 Aplikasi ini dikembangkan oleh tim yang berdedikasi dalam bidang Teknologi Artificial Intelligence. 
 Kami berkomumitmen akan terus mengembangkan apa yang sudah kami pelajari dengan baik.
 """)
-    
+
+# Foto pertama
+image1 = "Jackyyy.jpg"
+
+# Foto kedua
+image2 = "Jackyyy.jpg"
+
+# Foto ketiga
+image3 = "Jackyyy.jpg"
+
+# Ukuran foto
+image_width = 200
+
+# Buat kolom
+st.columns(3)
+
+# Tambahkan foto pertama ke kolom pertama
+st.column(1).image(image1, width=200)
+
+# Tambahkan foto kedua ke kolom kedua
+st.column(2).image(image2, width=200)
+
+# Tambahkan foto ketiga ke kolom ketiga
+st.column(3).image(image3, width=200)
+
 show_pages(
     [
-        Page("app.py", "Introduction"),
+        Page("app.py", "Pendahuluan"),
          Page("pages/ Penjelasan Segmentasi"),
     ]
 )
